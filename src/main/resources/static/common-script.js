78a447ecc4027c6bd03856581830d9130d4748d7
i9eRun <hyperjyw@gmail.com>
2025-04-04 10:24:27 +0900

    메뉴컨텐츠 로딩 오류 수정

--- src/main/resources/static/common-script.js
+++ src/main/resources/static/common-script.js
@@ -7,12 +7,12 @@ function loadProgram(menuId) {
     const scriptPath = `/app/view/${folder}/${lowerId}.js`;
   
     // 컴포넌트 이름 예: GoldWings.view.wina.WINA1003
-    const className = `GoldWings.view.${folder}.${menuId}`;
-  
+    const className = `GoldWings.view.${folder}.${lowerId}`;
+
+    contentPanel.removeAll(true);
     // 이미 정의된 경우에는 바로 생성
     if (Ext.ClassManager.isCreated(className)) {
       const cmp = Ext.create(className);
-      contentPanel.removeAll();
       contentPanel.add(cmp);
     } else {
       // 동적 로딩
@@ -20,7 +20,6 @@ function loadProgram(menuId) {
         url: scriptPath,
         onLoad: function () {
           const cmp = Ext.create(className);
-          contentPanel.removeAll();
           contentPanel.add(cmp);
         },
         onError: function () {
